34bd124ca8bae5ecdcb1f60272edcdb0babb9287
STEVE YANG <steve.yang@teguhsindo.com>
2018-05-21 19:50:14 +0700

    init

--- pdf_merger.py
+++ pdf_merger.py
@@ -30,53 +30,51 @@ def merge():
     output = PdfFileMerger()
 
     output.append(PdfFileReader(open(files[0], "rb")))
-    output.append(PdfFileReader(open(files[1], "rb")))                                    
+    output.append(PdfFileReader(open(files[1], "rb")))
 
-    output.write("output.pdf")
+    saved_file_name = filedialog.asksaveasfile(mode="wb", defaultextension=".pdf")
+    if saved_file_name is None:
+        return
+
+    output.write(saved_file_name)
 
 
 # Create application window and add frame -------
 window = Tk()
 window.title("PDF Merger")
-window.geometry("350x150")
+window.geometry("350x120")
 
-frame1 = Frame()
-frame1.grid(padx=5, pady=5)
-
-frame2 = Frame()
-frame2.grid(padx=5, pady=5)
-
-frame3 = Frame()
-frame3.grid(padx=5, pady=5)
+frame = Frame()
+frame.grid(padx=5, pady=5)
 
 
 # Create and add text labels --------------------
-label_file1 = Label(frame1, text="File 1")
-label_file1.grid(row=1, column=1, sticky=S+E)
+label_file1 = Label(frame, text="File 1")
+label_file1.grid(row=0, column=0)
 
-label_file2 = Label(frame2, text="File 2")
-label_file2.grid(row=2, column=1, sticky=S+E)
-
-
-# Blank label to hold name of chose file --------
-label_file1_name = Label(frame1)
-label_file1_name.grid(row=1, column=3)
-
-label_file2_name = Label(frame2)
-label_file2_name.grid(row=2, column=3)
+label_file2 = Label(frame, text="File 2")
+label_file2.grid(row=1, column=0)
 
 
 # Create buttons for 'Open' file dialog ---------
-button_open_file1 = Button(frame1, text="Choose PDF file 1", command=open_file1)
-button_open_file1.grid(row=1, column=2)
+button_open_file1 = Button(frame, text="Choose PDF file 1", command=open_file1)
+button_open_file1.grid(row=0, column=2, padx=2, pady=2)
 
-button_open_file2 = Button(frame2, text="Choose PDF file 2", command=open_file2)
-button_open_file2.grid(row=2, column=2)
+button_open_file2 = Button(frame, text="Choose PDF file 2", command=open_file2)
+button_open_file2.grid(row=1, column=2, padx=2, pady=2)
+
+
+# Blank label to hold name of chose file --------
+label_file1_name = Label(frame, width=25)
+label_file1_name.grid(row=0, column=3)
+
+label_file2_name = Label(frame, width=25)
+label_file2_name.grid(row=1, column=3)
 
 
 # Create and add button for merging -------------
-button_merge = Button(frame3, text="Merge", command=merge)
-button_merge.grid(row=3, column=2, columnspan=2)
+button_merge = Button(frame, text="Merge", command=merge)
+button_merge.grid(row=2, column=2, padx=2, pady=2, sticky=N+S+E+W)
 
 
 window.mainloop()
